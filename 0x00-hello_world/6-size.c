f41fce346d10de92e81798d94c213c0a40cb934d
Peter Torki <Peter@gmail.com>
2023-03-16 18:00:03 -0700

    Files 000a

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,9 @@
 /**
  * main - entry point
  *
- * Description: print the size of a data type in bytes
- *
+ * Description:  print the size of a data type in bytes
  * Return: 0 means there is no errors 
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
