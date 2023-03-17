8a161ca9e0b092c7f3c8aa4fad7cf96e5963281f
Peter Torki <Peter@gmail.com>
2023-03-17 16:53:14 -0700

    Ya22

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,10 @@ int main(void)
 {
     char c;
 
-    for (c = 'a'; c <= 'z'; c++) {
-        putchar(c);
-        putchar('\n');
-    }
+    for (c = 'a'; c <= 'z'; c++)
+    	putchar(c);
+    
+    putchar('\n');
+    
     return (0);
 }
