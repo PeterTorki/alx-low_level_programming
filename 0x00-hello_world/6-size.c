a7de0d502bfd69f66bf4825039bd455c5c6e8f50
Peter Torki <Peter@gmail.com>
2023-03-16 17:52:46 -0700

    Files 000

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  * Return: 0 means there is no errors 
  */
 
-int main()
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
