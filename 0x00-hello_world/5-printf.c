f3ceca3f3a00d0bb22121e52c189d2c0e9e7288b
Peter Torki <Peter@gmail.com>
2023-03-16 17:51:42 -0700

    Files 000

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print  the program prints a message
+ *
+ * Description: using puts function to print a message
+ *
+ * Return: 0 means there is no errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/* value 0 means there is no errors */
+
+	return (0);
+}
