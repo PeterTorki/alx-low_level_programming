5d141da5ab963c6f03c7ba06792f4e53971f6128
Peter Torki <Peter@gmail.com>
2023-03-17 16:19:14 -0700

    First 2

---
+++ 1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, ldo;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ldo = n % 10;
+	printf("Last digit of %d is %d ", n, ldo);
+	if(ldo > 5)
+		printf("and is greater than 5");
+	else if(ldo == 0)
+		printf("and is 0");
+	else
+		printf("and is less than 6 and not 0");
+	return (0);
+}
