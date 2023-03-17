2006a197df5c6761f18d83933c18987ea4f0b586
Peter Torki <Peter@gmail.com>
2023-03-16 17:36:49 -0700

    File 00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
+// kolena 3arfen bt3ml eh f fakes shr7
 int main(void)
 {
+	// Aywa kolna 3arfeen
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
