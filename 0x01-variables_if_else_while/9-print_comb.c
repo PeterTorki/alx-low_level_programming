49bd89e23075c97c0f8cc4aed9d3377d9368bc82
Peter Torki <Peter@gmail.com>
2023-03-17 17:28:06 -0700

    Yala 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0 means no errors
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++) 
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
