5f4e0612312b2782f5e018cd54bfa2e30f2f0537
Peter <peter.j.torki@gmail.com>
2023-03-21 17:30:08 +0200

    Filwee

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints the alphabet, in lowercase, followed by a new line 10 times.
+ * print_alphabet_x10 - prints the alphabet, in lowercase.
  * Return: 0
  */
 void print_alphabet_x10(void)
@@ -10,7 +10,9 @@ void print_alphabet_x10(void)
     for (i = 0; i < 10; i++)
     {
         for (c = 'a'; c <= 'z'; c++)
+        {
 	    	_putchar(c);
+        }
         _putchar('\n');
     }
 }
