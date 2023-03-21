2cbda3a7a51c9f47fc7e3af9e1f00e136d4071b4
Peter Torki <Peter@gmail.com>
2023-03-21 05:49:50 -0700

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0 means no errors
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
