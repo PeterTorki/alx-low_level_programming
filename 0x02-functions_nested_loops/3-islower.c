f7de00e837e9ece6a3296a04ad2ad3948ca45f45
Peter <peter.j.torki@gmail.com>
2023-03-22 03:03:15 +0200

    Ya3m Yalla

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 #include <ctype.h>
 /**
  * _islower - function returns 1 if c is lowercase
- * 
+ *
  * Return: <return>
  */
 int _islower(int c)
 {
-	if (c <= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	return (0);
 }
