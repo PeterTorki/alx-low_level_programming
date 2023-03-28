f92d4cd1a0f6367ccc69269a39266f28fe32e96e
Peter <peter.j.torki@gmail.com>
2023-03-28 04:04:10 +0200

    fwefef

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: array of string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int l = 0, r;
+
+	while (s[r])
+		r++;
+	r--;
+	while (l < r)
+	{
+		char x = s[l];
+		s[l] = s[r];
+		s[r] = x;
+		l++, r--;
+	}
+}
