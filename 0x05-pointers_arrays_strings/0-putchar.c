c5ac4efb46f9eb6ee100706ba17beed9d4d0ee98
Peter <peter.j.torki@gmail.com>
2023-03-28 02:46:23 +0200

    FIle

---
+++ 0x05-pointers_arrays_strings/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print -> _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char *s = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
