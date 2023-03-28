4eb71b833861fecee427faadd9f0219aeeadfd02
Peter <peter.j.torki@gmail.com>
2023-03-28 02:55:06 +0200

    file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - a swap function
+ * @a: integer's address a
+ * @b: integer's address b
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int x = *a;
+    *a = *b;
+    *b = x;
+}
